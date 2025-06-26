34fff4576f9c61fef157e7f9dfe383fc84c2d3b9
Lord Elvis TECH <antidevice203@gmail.com>
2025-06-26 10:35:26 +0100

    Update rw-tool.js

--- plugins/rw-tool.js
+++ plugins/rw-tool.js
@@ -17,7 +17,7 @@ cmd({
     const { data } = await axios.get(apiUrl);
     
     if (data.status && data.imgUrl) {
-      const caption = `🌌 *Random Wallpaper: ${query}*\n\n> *© Powered by Zuko*`;
+      const caption = `🌌 *Random Wallpaper: ${query}*\n\n> *© Powered by Chris*`;
       await conn.sendMessage(from, { image: { url: data.imgUrl }, caption }, { quoted: m });
     } else {
       reply(`❌ No wallpaper found for *"${query}"*.`);
